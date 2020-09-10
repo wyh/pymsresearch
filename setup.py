5430d7ccd1815c8c0c5988677cbbc4dbd8c72166
Samuel.Wu <wyh@ivysci.com>
2020-09-10 08:09:40 +0000

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='researchacademic',
-    version='0.8.1',
+    version='0.8.2',
     description=("A python library that aims to retrieve data from Microsoft"
                  "Research Academic and parse data to human readable"),
     long_description=long_description,
